6028cd4218908182723d2f19a2bf835a0795c858
Maria Ma <i.am.maria.ma@gmail.com>
2021-03-14 11:51:51 -0700

    update readme

--- README.md
+++ README.md
@@ -2,6 +2,35 @@
 Generating synthetic health data with an agent based model
 
 
+[Synthea](https://github.com/synthetichealth/synthea) is great if you have/ want really in depth patient records. But, sometimes you don't get stuff in that much detail. This is an attempt to generate synthetic health data for settings where you kinda don't have that much information. 
 
-Synthea is great if you have/ want really in depth patient records. But, sometimes you don't get stuff in that much detail. This is an attempt to generate synthetic health data for settings where you kinda don't have that much information. 
+
+goal is synthetic health data in a rural community health setting 
+
+
+
+## to do 
+* add in age, wealth, education, health visits & kids vars 
+* update birth into facility vs home births
+
+* add in danger signs + interaction with age + kids + health visits
+* add deaths
+
+
+* figure out how to get it to give me the state of each individual at each timestep instead of the number of individuals in each state
+
+
+
+# log
+* 2020-03-14 - pregnancies, births, miscarriages are modeled. 
+
+
+
+
+## refs
+* [Doctor, H.V., Nkhana-Salimu, S. & Abdulsalam-Anibilowo, M. Health facility delivery in sub-Saharan Africa: successes, challenges, and implications for the 2030 development agenda. BMC Public Health 18, 765 (2018).](https://bmcpublichealth.biomedcentral.com/articles/10.1186/s12889-018-5695-z/tables/4)
+  *  limitation: paper assumes no interaction btw variables. obvs not true given that wealth + education are usually correlated. but whatever this is the best data i have right now anyway
+
+
+* [Population Division of the United Nations Department of Economic and Social Affairs. Potential impact of later childbearing on future population. (2019)] (https://www.un.org/en/development/desa/population/publications/pdf/popfacts/PopFacts_2019-5.pdf)
 
--- model.R
+++ model.R
@@ -21,7 +21,7 @@ tmax <- 365*2
 steps <- tmax/dt
 
 
-health_states <- c("O", "P", "B") #, "M", "C", "H", "D")
+health_states <- c("O", "P", "B", "M") #, "M", "C", "H", "D")
 health_states_t0 <- rep("O", N) # everyone starts off not pregnant
 
 #P0 <- 0
@@ -29,14 +29,13 @@ health_states_t0 <- rep("O", N) # everyone starts off not pregnant
 # N = nOt pregnant, P = Pregnant, M = miscarried, C = faCility birth, H = Home birth D = death
 health <- CategoricalVariable$new(categories = health_states,initial_values = health_states_t0)
 
+# vars
 
 #age <- IntegerVariable$new(runif(N, 15, 51)) # should i just make this categorical? 
 #wealth_quintile <- IntegerVariable$new(runif(N, 1,5)) 
 #education <- IntegerVariable$new(runif(N, 0,1)) # 0 = no education, 1 = education 
 #gets_ANC_visits
 #existing_kids <- 
-  
-# vars
 # let's say all 100 mothers are prgnant in a year, on average, so risk f pregnancy is 1/365
 # every day, the mother risks developing danger signs
 # younger mothers + older mothers are at higher risk for danger signs
@@ -55,6 +54,10 @@ health <- CategoricalVariable$new(categories = health_states,initial_values = he
 # At birth, mother will either have a facility delivery or not.
 
 # If a facility delivery + danger signs, death risk is lower. 
+
+
+
+
 birth_timer_t0 <- rep(273, N)
 
 birth_timer <- IntegerVariable$new(birth_timer_t0)
@@ -62,14 +65,15 @@ birth_timer <- IntegerVariable$new(birth_timer_t0)
 # Processes
 
 pregnancy_process <- function(timestep) {
-  
   P <- health$get_size_of("O")
   O <- health$get_index_of("O")
-  O$sample(rate = pexp(q = preg_rate))
+  O$sample(rate = preg_rate)
   health$queue_update(value = "P",index = O)
   
 }
-# miscarriage_process <- 
+miscarriage_process <- bernoulli_process(health, "P", "M", rate = 0.1/273)
+
+
 
 birth_process <- function(timestep) {
   pregnant_people <- health$get_index_of("P")
@@ -105,7 +109,7 @@ health_render_process <- categorical_count_renderer_process(
 
 simulation_loop(
   variables = list(health),
-  processes = list(pregnancy_process, birth_process, birth, health_render_process),
+  processes = list(pregnancy_process, miscarriage_process, birth_process, birth, health_render_process),
   events = list(pregnancy_event),
   timesteps=365*2
 )
@@ -114,18 +118,26 @@ simulation_loop(
 
 
 states <- health_render$to_dataframe()
-health_cols <-  c("royalblue3","firebrick3","darkorchid3")
-matplot(
-  x = states[[1]]*dt, y = states[-1],
-  type="l",lwd=2,lty = 1,col = adjustcolor(col = health_cols, alpha.f = 0.85),
-  xlab = "Time",ylab = "Count"
-)
-legend(
-  x = "topright",pch = rep(16,3),
-  col = health_cols,bg = "transparent",
-  legend = health_states, cex = 1.5
-)
 
+library(ggplot2)
+library(tidyverse)
+states %>%
+  gather(state, var, -timestep) %>% 
+  ggplot() + geom_line(aes(x=timestep, y= var, color = state))
+ 
+
+# health_cols <-  c("royalblue3","firebrick3","darkorchid3", "black")
+# matplot(
+#   x = states[[1]]*dt, y = states[-1],
+#   type="l",lwd=2,lty = 1,col = adjustcolor(col = health_cols, alpha.f = 0.85),
+#   xlab = "Time",ylab = "Count"
+# )
+# legend(
+#   x = "topright",pch = rep(16,3),
+#   col = health_cols,bg = "transparent",
+#   legend = health_states, cex = 1.5
+# )
+# 
 
 # 
 
